a88a511f1909c9b7c0ccbc2063fba1e58e2e4561
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-17 13:24:06 +0530

    refactor: Footer

--- client/src/components/layouts/Footer.tsx
+++ client/src/components/layouts/Footer.tsx
@@ -1,21 +1,27 @@
-"use client"
+"use client";
 
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
 import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
 
 export default function Footer() {
   return (
-    <Box sx={{ py: 3, backgroundColor: '#1976d2', color: 'white' }}>
-      <Container maxWidth="lg" sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+    <Box sx={{ py: 3, backgroundColor: "#1976d2", color: "white" }}>
+      <Container
+        maxWidth="lg"
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <Typography variant="body2" component="p">
-          © 2024 Your Company Name. All rights reserved.
+          © 2024 Quadzilla. All rights reserved.
         </Typography>
         <Box>
           <IconButton aria-label="facebook" color="inherit">
