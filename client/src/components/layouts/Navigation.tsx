11bdef51cbb6e339a5f8917e3a600d46d869d231
SMCN Karunarathna <130365250+charukaonline@users.noreply.github.com>
2024-05-17 10:42:34 +0530

    Revert "nav bar added"

--- client/src/components/layouts/Navigation.tsx
+++ client/src/components/layouts/Navigation.tsx
@@ -1,37 +1,19 @@
-"use client"
+import { useSession, signIn, signOut } from "next-auth/react"
 
-import * as React from "react";
-import { useNavigate } from "react-router-dom";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-
-export default function ButtonAppBar() {
-  const navigate = useNavigate();
-
-  const handleSignInClick = () => {
-    navigate("@/app/(pages)/(auth)/signin/page");
-  };
-
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Logo
-          </Typography>
-          <Button variant="outlined" color="inherit" sx={{ marginRight: 1 }} onClick={handleSignInClick}>
-            Sign In
-          </Button>
-          <Button variant="outlined" color="inherit" sx={{ marginRight: 1 }}>
-            Sign Up
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
 }
